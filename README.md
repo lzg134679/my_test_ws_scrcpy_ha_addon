71761af6e1e04afa08558455fcacaf9b75ccaf59
lzg134679 <15868833157@163.com>
2026-03-15 12:27:02 +0800

    1.0.2

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # Scrcpy Web
 
 1. 进入【配置】页面添加adb设备
-    ```yaml
-    hosts:
-      - 192.168.1.100:5555
-      - 192.168.1.200:5555
-    ```
+   ```yaml
+   hosts:
+     - 192.168.1.100:5555
+     - 192.168.1.200:5555
+   ```
 2. 重启加载项
 3. 在安卓设备上同意调试
 
 # 替换文件说明
-1. 允许在ha配置文件中更换自定义文件
-2. 配置文件路径：/config/ws-scrcpy（如不存在需自行新建）
+
+1. 允许在ha的加载项配置文件中更换自定义文件
+2. 配置文件路径：/addon_configs/xxxxxxxx_scrcpy/ws-scrcpy（如不存在需运行一次加载项，会自动生成）
 3. 自定义文件会替换容器内的对应路径文件
-4. 例如需要替换/src/style/app.css，则将修改后的app.css放入/config/ws-scrcpy/src/style/app.css
+4. 例如需要替换/src/style/app.css，则将修改后的app.css放入/addon_configs/xxxxxxxx_scrcpy/ws-scrcpy/src/style/app.css
 5. 重启加载项后对应文件将替换进docker容器内的/app/src/style/app.css
+
--- rootfs/run.sh
+++ rootfs/run.sh
@@ -5,16 +5,14 @@ ingress_entry=$(bashio::addon.ingress_entry)
 set -ex
 
 # Check if custom files exist and copy them
-echo "检查/config/ws-scrcpy目录..."
+echo "检查加载项addon_configs目录..."
 ls -la /config/ || echo "无法列出/config目录"
 if [ -d /config/ws-scrcpy ]; then
-    echo "在配置文件/config/ws-scrcpy中发现源文件, 复制替换进容器/app目录"
-    ls -la /config/ws-scrcpy/ || echo "无法列出/config/ws-scrcpy目录"
+    echo "在加载项配置目录/config/ws-scrcpy/中发现源文件, 复制替换进容器/app目录"
     cp -r /config/ws-scrcpy/* /app/
-    echo "复制完成，检查/app目录..."
-    ls -la /app/src/style/ || echo "无法列出/app/src/style目录"
+    echo "复制完成"
 else
-    echo "未在配置文件/config/ws-scrcpy中发现源文件, 使用默认文件"
+    echo "未在加载项配置目录/config/ws-scrcpy/中发现源文件, 使用默认文件运行"
 fi
 
 sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/http.d/*.conf
